36dbc614c3cb2b100d02fcc5134d0887d9f2ccdb
Abu Kasozi <abukasozi@gmail.com>
2022-01-11 10:16:07 +0300

    Added support for edge cases

--- src/main/java/com/motionlab/strive/arrays101/MaxConsecutiveOnes.java
+++ src/main/java/com/motionlab/strive/arrays101/MaxConsecutiveOnes.java
@@ -16,6 +16,9 @@ public class MaxConsecutiveOnes {
      */
     public int findMaxConsecutiveOnes(int[] nums) {
 
+        if (nums == null || nums.length == 0)
+            return 0;
+
         int max = 0; int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
